f8379f0bc5cfc7b123e7aa060f8216665c15a542
Nova <nova@vitacoin.dev>
2026-04-20 13:59:31 +0530

    fix: update VITAPAY to vitacoin-testnet-2 + vita prefix

--- vitapay-mobile/src/constants/config.ts
+++ vitapay-mobile/src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
-  CHAIN_ID: 'vitacoin-testnet-1',
+  CHAIN_ID: 'vitacoin-testnet-2',
   RPC_ENDPOINT: 'http://rpc.vitacoin.network',
   REST_ENDPOINT: 'http://api.vitacoin.network',
   DENOM: 'uvita',
   DISPLAY_DENOM: 'VITA',
   DENOM_DECIMALS: 6,
-  BECH32_PREFIX: 'cosmos',
+  BECH32_PREFIX: 'vita',
   GAS_PRICE: '0.025uvita',
   APP_NAME: 'VITAPAY',
   VERSION: '1.0.0-testnet',
